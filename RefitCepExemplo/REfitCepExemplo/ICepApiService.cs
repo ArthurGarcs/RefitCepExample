685c5272072acb5a6d8b450306009e3979beb0a8
Arthur Garcia <138077549+ArthurGarcs@users.noreply.github.com>
2024-05-08 17:00:28 -0300

    Add files via upload

---
+++ RefitCepExemplo/REfitCepExemplo/ICepApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+
+namespace RefitCepExample
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        //descrevemos aqui o método da APi que usaremos e ja deixamos o espaço 
+        //para colocação do cep no corpo do endereço, que sera substituido em tempo de executação pelo cep 
+        //que for passado como parametro
+        Task<CepResponse> GetAddressAsync(string cep);
+        //Aqui é declarado como sera realizado o chamado para a api
+        //vemos na assinatura do método que é mencionada a Classe
+        //“CepResponse”, como o item que será retornado, já o método GetAddressAsync, recebe um parâmetro do tipo “string” que será inserido no endereço escrito na linha anterior.
+    }
+}
+
---
+++ RefitCepExemplo/REfitCepExemplo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+
+namespace RefitCepExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+            //declara o objeto cepClient que sera isntalado pelo Refit como um serviço que possua 
+            //os metodos da interface ICepApiService 
+            string meuCep = "13300390";
+            Console.WriteLine("Consultando dados do Cep:" + meuCep);
+
+
+            var address = await cepClient.GetAddressAsync(meuCep);
+            // nesta linha é criado um objweto "address" atráves do retorno do metodo GetAddressAsync com o cep informado 
+
+
+
+            Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}" +
+                $"\nEstado:{address.Uf}\nCódigo Ibge:{address.Ibge}");
+            Console.ReadKey();
+        }
+    }
+}
